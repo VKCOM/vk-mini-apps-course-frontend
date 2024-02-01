00800ac616336e595b6b08fbb8d3f73e6e9d1aa2
aleksey.pronin <aleksey.pronin@vk.team>
2024-02-01 15:54:52 +0300

    lesson_1: update project settings

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import ReactDOM from 'react-dom/client';
 import vkBridge from '@vkontakte/vk-bridge';
 import { AppConfig } from './AppConfig.tsx';
+import eruda from 'eruda';
 
 vkBridge.send('VKWebAppInit');
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<AppConfig />);
 
 if (import.meta.env.MODE === 'development') {
-  import('./eruda.ts');
+  eruda.init();
 }
